0afd7eccc139f7662973addc01fdbdadb529c927
abdulrahmanmokhlef <abdulrahman.mokhlef@gmail.com.com>
2021-04-08 14:37:17 +0200

    change the server prot to 8081

--- projects/evaluate-news-nlp/src/client/js/formHandler.js
+++ projects/evaluate-news-nlp/src/client/js/formHandler.js
@@ -1,12 +1,12 @@
 function handleSubmit(event) {
     event.preventDefault()
-
+    debugger
     // check what text was put into the form field
     let formText = document.getElementById('name').value
     Client.checkForName(formText)
-
+    debugger
     console.log("::: Form Submitted :::")
-    fetch('http://localhost:8080/test')
+    fetch('http://localhost:8081/test')
     .then(res => res.json())
     .then(function(res) {
         document.getElementById('results').innerHTML = res.message
